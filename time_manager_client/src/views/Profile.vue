a274c659b5f8e76bd4b72f75d8a093be496d94e8
gconreaux <gregoire.conreaux@pc.com>
2020-11-06 10:30:53 +0100

    Correction de bug changement employee dashboard

--- time_manager_client/src/views/Profile.vue
+++ time_manager_client/src/views/Profile.vue
@@ -94,7 +94,7 @@ export default {
     periode: "Weekly",
     options: {
       responsive: true,
-      maintainAspectRatio: false
+      maintainAspectRatio: false,
     },
     visualizeOptions: ["Line", "Bar"],
     periodeOptions: ["Weekly", "Monthly"],
@@ -110,11 +110,33 @@ export default {
       "September",
       "October",
       "November",
-      "December"
-    ]
+      "December",
+    ],
   }),
   methods: {
     ...mapMutations("user", ["setId", "setEmail", "setUsername"]),
+    init() {
+      AccountService.getUserInfos(this.userId ? this.userId : this.id).then(
+        (res) => {
+          this.dailyWorkingtimes = res.dailyWorkingtimes;
+          this.dailyClocks = res.dailyClocks;
+          this.isInworkingtime = res.isInworkingtime;
+          this.currentWorkingTime = res.currentWorkingTime;
+          this.weeklyWorkingtimes = res.weeklyWorkingtimes;
+          this.weeklyClocks = res.weeklyClocks;
+          this.weeklyClocks = res.sortClockByWeekDay;
+          this.monthlyWorkingtimes = res.monthlyWorkingtimes;
+          this.monthlyClocks = res.monthlyClocks;
+          this.monthlyClocks = res.sortClockByMonthDay;
+
+          this.setDailyWorkingtimeChart(
+            this.currentWorkingTime,
+            this.dailyClocks
+          );
+          this.setWeeklyWorkingtimeChart(this.weeklyClocks);
+        }
+      );
+    },
     getPlanning(workingtime) {
       let start = new Date(workingtime.start);
       let end = new Date(workingtime.end);
@@ -134,7 +156,7 @@ export default {
     setDailyWorkingtimeChart(currentWorkingTime, dailyClocks) {
       if (currentWorkingTime) {
         let lastClock = dailyClocks.find(
-          clock =>
+          (clock) =>
             (new Date(clock.start) >= new Date(currentWorkingTime.start) &&
               !clock.end) ||
             (clock.end &&
@@ -169,9 +191,9 @@ export default {
                   {
                     data: [late, work, (total - work).toFixed(2)],
                     backgroundColor: ["yellow", "green"],
-                    weight: 0.5
-                  }
-                ]
+                    weight: 0.5,
+                  },
+                ],
               }
             : {
                 labels: ["Start margin", "Workingtime", "End margin"],
@@ -179,9 +201,9 @@ export default {
                   {
                     data: [late, work, (total - work - late).toFixed(2)],
                     backgroundColor: ["yellow", "green", "yellow"],
-                    weight: 0.5
-                  }
-                ]
+                    weight: 0.5,
+                  },
+                ],
               };
         } else {
           let late = (
@@ -195,9 +217,9 @@ export default {
               {
                 data: [late, total - late],
                 backgroundColor: ["yellow"],
-                weight: 0.5
-              }
-            ]
+                weight: 0.5,
+              },
+            ],
           };
         }
       }
@@ -211,11 +233,11 @@ export default {
           "Thursday",
           "Friday",
           "Saturday",
-          "Sunday"
+          "Sunday",
         ],
         datasets: [
           {
-            data: sortClockByDay.map(dayClocks =>
+            data: sortClockByDay.map((dayClocks) =>
               dayClocks
                 .reduce((hours, clock) => {
                   if (clock.end) {
@@ -234,9 +256,9 @@ export default {
             ),
             label: "Workingtime",
             backgroundColor: "green",
-            weight: 0.5
-          }
-        ]
+            weight: 0.5,
+          },
+        ],
       };
     },
     setMonthWorkingtimeChart(sortClockByMonth) {
@@ -244,7 +266,7 @@ export default {
         labels: this.monthNames,
         datasets: [
           {
-            data: sortClockByMonth.map(monthocks =>
+            data: sortClockByMonth.map((monthocks) =>
               monthocks
                 .reduce((hours, clock) => {
                   if (clock.end) {
@@ -265,37 +287,18 @@ export default {
             ),
             label: "Workingtime",
             backgroundColor: "green",
-            weight: 0.5
-          }
-        ]
+            weight: 0.5,
+          },
+        ],
       };
-    }
+    },
   },
 
   computed: {
-    ...mapState("user", ["id", "email", "username"])
+    ...mapState("user", ["id", "email", "username"]),
   },
   mounted() {
-    AccountService.getUserInfos(this.userId ? this.userId : this.id).then(
-      res => {
-        this.dailyWorkingtimes = res.dailyWorkingtimes;
-        this.dailyClocks = res.dailyClocks;
-        this.isInworkingtime = res.isInworkingtime;
-        this.currentWorkingTime = res.currentWorkingTime;
-        this.weeklyWorkingtimes = res.weeklyWorkingtimes;
-        this.weeklyClocks = res.weeklyClocks;
-        this.weeklyClocks = res.sortClockByWeekDay;
-        this.monthlyWorkingtimes = res.monthlyWorkingtimes;
-        this.monthlyClocks = res.monthlyClocks;
-        this.monthlyClocks = res.sortClockByMonthDay;
-
-        this.setDailyWorkingtimeChart(
-          this.currentWorkingTime,
-          this.dailyClocks
-        );
-        this.setWeeklyWorkingtimeChart(this.weeklyClocks);
-      }
-    );
+    this.init();
   },
   watch: {
     userId: function(newVal) {
@@ -310,13 +313,13 @@ export default {
       this.dailyWorkData = null;
       this.weeklyWorkData = null;
       this.init();
-    }
+    },
   },
   components: {
     LineChart,
     DoughnutChart,
-    BarChart
-  }
+    BarChart,
+  },
 };
 </script>
 
